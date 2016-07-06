741fb67e33da3928815858aaa500fd6b1497bbc4
wooken <kenny@kenwoo.io>
2016-07-05 19:00:20 -0700

    Move python functionality out of global scope

--- autoload/leaders.py
+++ autoload/leaders.py
@@ -1,11 +1,16 @@
 import os
 import re
 
-with open(os.path.expanduser('~') + '/.vimrc', 'r') as f:
-    vimrc = f.read()
 
-matches = re.findall('[a-z]+map <leader>[^\s]+ [a-zA-Z0-9<>:]+', vimrc)
+def main():
+    with open(os.path.expanduser('~') + '/.vimrc', 'r') as vimrc_fd:
+        vimrc = vimrc_fd.read()
 
-for match in matches:
-    parts = match.split(' ')
-    print('{} {}'.format(parts[1], parts[2]))
+    matches = re.findall(r'[a-z]+map <leader>[^\s]+ [a-zA-Z0-9<>:]+', vimrc)
+
+    for match in matches:
+        parts = match.split(' ')
+        print('{} {}'.format(parts[1], parts[2]))
+
+if __name__ == '__main__':
+    main()
