2a82485a49ff6378e8723d05d43c2786801b4e69
wooken <kenny@kenwoo.io>
2016-07-12 00:35:57 -0700

    Remove in-line comment regex
    Unsupported by vim

--- autoload/leaders.py
+++ autoload/leaders.py
@@ -6,7 +6,6 @@ MAP_RE = '[a-z]*map'
 LEADER_RE = '<[L|l][E|e][A|a][D|d][E|e][R|r]>'
 MAPPING_RE = r'[^\s]+'
 CMD_RE = '[a-zA-Z0-9<>:]+'
-COMMENT_RE = r'"\s+[a-z ]+'
 
 
 def extract_mapping_and_info(item: str) -> list:
@@ -22,7 +21,7 @@ def extract_mapping_and_info(item: str) -> list:
 
 
 def is_map_function(text: str) -> bool:
-    map_function_re = MAP_RE + r'\s+' + LEADER_RE + MAPPING_RE + r'\s+' + CMD_RE + r'(\s+' + COMMENT_RE + ')?'
+    map_function_re = MAP_RE + r'\s+' + LEADER_RE + MAPPING_RE + r'\s+' + CMD_RE
     is_valid = re.match(map_function_re, text)
     return bool(is_valid)
 
@@ -40,7 +39,6 @@ def main():
         info = extract_mapping_and_info(match)
         spaces = ' ' * (6 - len(info[0]))
         print('{}{}| {}'.format(info[0], spaces, info[1]))
-
 
 if __name__ == '__main__':
     main()
